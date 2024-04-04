ed9a19e18967b891afe551a2c678ee98818249f0
Aarush@122015 <you@example.com>
2024-04-04 05:41:35 +0530

    adding new pattern BuilderPattern under section on Creational pattern

---
+++ DesignPatterns/Creational/BuilderPattern/Document.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.DesignPatterns.Creational.BuilderPattern
+{
+    public class Document
+    {
+        public string Title { get; private set; }
+        public string Content { get; private set; }
+        public Dictionary<string, string> Metadata { get; private set; } = new Dictionary<string, string>();
+        public List<string> Permissions { get; private set; } = new List<string>();
+        public DocumentType Type { get; private set; }
+
+        // Builder class
+        public class DocBuilder
+        {
+            private readonly Document _document = new Document();
+
+            public DocBuilder WithTitle(string title)
+            {
+                _document.Title = title;
+                return this;
+            }
+
+            public DocBuilder WithContent(string content)
+            {
+                _document.Content = content;
+                return this;
+            }
+
+            public DocBuilder WithMetadata(string key, string value)
+            {
+                _document.Metadata.Add(key, value);
+                return this;
+            }
+
+            public DocBuilder WithPermission(string permission)
+            {
+                _document.Permissions.Add(permission);
+                return this;
+            }
+
+            public DocBuilder WithType(DocumentType type)
+            {
+                _document.Type = type;
+                return this;
+            }
+
+            public Document Build()
+            {
+                return _document;
+            }
+        }
+    }
+
+    public enum DocumentType
+    {
+        Word,
+        PDF,
+        Excel
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DesignPatterns.Creational.FactoryPattern;
 using DesignPatterns.DesignPatterns.Creational.AdapterPattern;
+using DesignPatterns.DesignPatterns.Creational.BuilderPattern;
 
 Console.WriteLine("Hello, Lets Test our design pattern's code.!");
 Console.WriteLine("Test Creational design pattern started.!");
@@ -17,9 +18,29 @@ string type=Console.ReadLine().ToString();
 IDocument wordDoc = DocumentFactory.GetDocument(type);
 wordDoc.CreateDocument();
 wordDoc.OpenDocument("path/to/word/document");
-
+Console.WriteLine();
+Console.WriteLine("Adapter design pattern started.!");
+Console.WriteLine();
 IConnection connection = new ConnectionClass("connection-string-here");
 connection.Connect();
 connection.Execute("SELECT * FROM Users");
 connection.Disconnect();
+Console.WriteLine();
+Console.WriteLine("Bulder design pattern started.!");
+Console.WriteLine();
+//var document = new DesignPatterns.DesignPatterns.Creational.BuilderPattern.Document.DocBuilder()
+//    .WithTitle("Example")
+//    .WithContent("This is an example.")
+//    .Build();
 
+Document.DocBuilder builder = new Document.DocBuilder();
+
+// Chain methods to set properties of the document
+Document myDocument = builder
+                      .WithTitle("My Document Title")
+                      .WithContent("This is the content of the document.")
+                      .Build(); // Finalize and get the Document object
+
+// The document object is now constructed with the specified properties
+Console.WriteLine($"Document Title: {myDocument.Title}");
+Console.WriteLine($"Document Content: {myDocument.Content}");
